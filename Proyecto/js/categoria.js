03d5fb6229292bbb45a4c920f07b61e644b94ae9
RaluRive <ralucrome@gmail.com>
2026-05-23 19:42:51 +0200

    Delete Proyecto directory

--- Proyecto/js/categoria.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    let botones=document.querySelectorAll(".categorias__list>button");
-    botones.forEach(boton => {
-        boton.addEventListener("click",()=>{
-            boton.parentElement.querySelector("p").classList.toggle("mostrando__reglas");
-        })
-    });
-})
